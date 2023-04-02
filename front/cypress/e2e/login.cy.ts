049b5c983f682c3da0f1023b6cedb5b538863c77
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2023-04-03 00:27:30 +0200

    split e2e tests

--- front/cypress/e2e/login.cy.ts
+++ front/cypress/e2e/login.cy.ts
@@ -1,69 +1,73 @@
 /// <reference types="cypress" />
 
-describe('Login spec', () => {
-    it('Should login successfull', () => {
+export function run(){
 
-        cy.visit('/login');
-
-        cy.intercept('POST', '/api/auth/login', {
-            body: {
-                id: 1,
-                username: 'userName',
-                firstName: 'firstName',
-                lastName: 'lastName',
-                admin: true
-            },
+    describe('Login spec', () => {
+        it('Should login successfull', () => {
+    
+            cy.visit('/login');
+    
+            cy.intercept('POST', '/api/auth/login', {
+                body: {
+                    id: 1,
+                    username: 'userName',
+                    firstName: 'firstName',
+                    lastName: 'lastName',
+                    admin: true
+                },
+            });
+    
+            cy.intercept('GET', '/api/session', []).as('session');
+    
+            cy.get('input[formControlName=email]').type("me@not-me.com");
+            cy.get('input[formControlName=password]').type("azerty{enter}{enter}");
+    
+            cy.url().should('include', '/sessions');
+    
         });
-
-        cy.intercept('GET', '/api/session', []).as('session');
-
-        cy.get('input[formControlName=email]').type("me@not-me.com");
-        cy.get('input[formControlName=password]').type("azerty{enter}{enter}");
-
-        cy.url().should('include', '/sessions');
-
+    
+        it('Should logout', () => {
+    
+            cy.url().should('include', '/sessions');
+            cy.get('span[data-test-id=logout-btn]').click();
+            cy.url().should('not.contain', '/sessions');
+    
+        });
+    
+        it('Should error if wrong credential', () => {
+            
+            cy.visit('/login');
+    
+            cy.intercept('POST', '/api/auth/login', { statusCode: 401});
+    
+            cy.get('input[formControlName=email]').type("me@not-me.com");
+            cy.get('input[formControlName=password]').type("azerty{enter}{enter}");
+    
+            cy.url().should('not.contain', '/sessions');
+    
+            cy.get('p').should('exist');
+    
+        });
+    
+        it('Shoud error if missing input email', () => {
+    
+            cy.visit('/login');
+    
+            cy.get('input[formControlName=password]').type("azerty{enter}{enter}");
+    
+            cy.get('input[formControlName=email]').should('have.class', 'ng-invalid');
+    
+        });
+    
+        it('Shoud submit disabled if missing passord', () => {
+    
+            cy.visit('/login');
+    
+            cy.get('input[formControlName=email]').type("me@not-me.com");
+    
+            cy.get('button[type=submit]').should('be.disabled');
+    
+        });
     });
-
-    it('Should logout', () => {
-
-        cy.url().should('include', '/sessions');
-        cy.get('span[data-test-id=logout-btn]').click();
-        cy.url().should('not.contain', '/sessions');
-
-    });
-
-    it('Should error if wrong credential', () => {
-        
-        cy.visit('/login');
-
-        cy.intercept('POST', '/api/auth/login', { statusCode: 401});
-
-        cy.get('input[formControlName=email]').type("me@not-me.com");
-        cy.get('input[formControlName=password]').type("azerty{enter}{enter}");
-
-        cy.url().should('not.contain', '/sessions');
-
-        cy.get('p').should('exist');
-
-    });
-
-    it('Shoud error if missing input email', () => {
-
-        cy.visit('/login');
-
-        cy.get('input[formControlName=password]').type("azerty{enter}{enter}");
-
-        cy.get('input[formControlName=email]').should('have.class', 'ng-invalid');
-
-    });
-
-    it('Shoud submit disabled if missing passord', () => {
-
-        cy.visit('/login');
-
-        cy.get('input[formControlName=email]').type("me@not-me.com");
-
-        cy.get('button[type=submit]').should('be.disabled');
-
-    });
-});
+    
+}
--- front/cypress/e2e/profile.cy.ts
+++ front/cypress/e2e/profile.cy.ts
@@ -1,55 +1,59 @@
 /// <reference types="cypress" />
 
-describe('Profile spec', () => {
+export function run(){
+
+    describe('Profile spec', () => {
     
-    it('Should login successfull', () => {
-
-        cy.visit('/login');
-
-        cy.intercept('POST', '/api/auth/login', {
-            body: {
-                id: 1,
-                username: 'userName',
-                firstName: 'firstName',
-                lastName: 'lastName',
-                admin: true
-            },
-        });
-
-        cy.intercept('GET', '/api/session', []).as('session');
-
-        cy.get('input[formControlName=email]').type("me@not-me.com");
-        cy.get('input[formControlName=password]').type("azerty{enter}{enter}");
-
-        cy.url().should('include', '/sessions');
-
-    });
-
-    it('Should delete profile', () => {
-        cy.url().should('include', '/sessions');
-
-        cy.intercept('GET', '/api/user/1', {
-            body: [{
+        it('Should login successfull', () => {
+    
+            cy.visit('/login');
+    
+            cy.intercept('POST', '/api/auth/login', {
+                body: {
                     id: 1,
+                    username: 'userName',
                     firstName: 'firstName',
                     lastName: 'lastName',
-                    createdAt: '2023-03-19T00:00:00',
-                    updatedAt: '2023-03-19T00:00:00',
-                    email: 'me@not-me.com',
                     admin: true
-            }]
+                },
+            });
+    
+            cy.intercept('GET', '/api/session', []).as('session');
+    
+            cy.get('input[formControlName=email]').type("me@not-me.com");
+            cy.get('input[formControlName=password]').type("azerty{enter}{enter}");
+    
+            cy.url().should('include', '/sessions');
+    
         });
-  
-        cy.get('[routerLink=me]').click();
-        cy.url().should('include', '/me');
-        
-        cy.intercept('DELETE', '/api/user/1', {
-            status: 200
+    
+        it('Should delete profile', () => {
+            cy.url().should('include', '/sessions');
+    
+            cy.intercept('GET', '/api/user/1', {
+                body: [{
+                        id: 1,
+                        firstName: 'firstName',
+                        lastName: 'lastName',
+                        createdAt: '2023-03-19T00:00:00',
+                        updatedAt: '2023-03-19T00:00:00',
+                        email: 'me@not-me.com',
+                        admin: true
+                }]
+            });
+      
+            cy.get('[routerLink=me]').click();
+            cy.url().should('include', '/me');
+            
+            cy.intercept('DELETE', '/api/user/1', {
+                status: 200
+            });
+    
+            cy.get('button[data-test-id=delete-btn').click();
         });
-
-        cy.get('button[data-test-id=delete-btn').click();
+    
+    
+    
     });
 
-
-
-});
+}
